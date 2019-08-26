da68a96b34dbc823b04bb54517434a6cbf03d2f0
Ilaria Colangelo <ilaria.colangelo@mail.polimi.it>
2019-08-26 14:06:04 +0200

    addrel

--- main/main.c
+++ main/main.c
@@ -18,11 +18,11 @@
 
 typedef struct rel { char id[1024]; }idRel;
 
-typedef struct ent_orig {
+typedef struct ent_odest {
     char name[1024];
     int len_array;
     idRel rel[SIZETYPEREL];
-}ent_orig;
+}ent_odest;
 
 typedef struct ent_dest {
     char name[1024];
@@ -32,7 +32,7 @@ typedef struct ent_dest {
 typedef struct ent{                                 // entity in hashmap
     char name[1024];
     int len_array;
-    ent_orig orig[SIZEORIG];
+    ent_odest odest[SIZEORIG];
     struct ent *next;
 }entity;
 
@@ -83,6 +83,19 @@ void findInHash(char username[],entity **p) {
     }
 }
 
+void findRel(char id_rel[], typeRel **p) {
+    int pos=(int)id_rel[0]- 45;
+    (*p)=array_lex[pos];
+    if ((*p)!= NULL){
+        while ((*p)->next!=NULL && strcmp((*p)->id_rel,id_rel)<0){
+            (*p) = (*p)->next;
+        }
+        if(strcmp((*p)->name,username)!=0) {
+            (*p) = NULL;
+        }
+    }
+}
+
 void addent(entity *hash[]){
     char username[1024];
 
@@ -124,7 +137,12 @@ void addrel(){
     char dest[1024];
     char rel[1024];
 
-    int cmpRel;
+    int pos;
+    int cmpDest,cmpRel,cmpTRel;
+    int i=0;
+    int j=0;
+    int z=0;
+    int exist = 0;
 
     typeRel *prec=NULL;
     typeRel *pointer=NULL;
@@ -140,55 +158,106 @@ void addrel(){
     findInHash(dest,&p_dest);
 
     if (p_dest !=NULL && p_orig!=NULL) {
-        printf("\nREL: %s\n",rel);
-        pos = (int) rel[0] - 45; //ASCII value of first char admitted is 45
-        pointer=array_lex[pos];
 
-        while (pointer != NULL && pointer->next != NULL && strcmp(pointer->id_rel,rel)<0) {
-            prec = pointer;
-            pointer = pointer->next;
+        cmpDest = strcmp(p_orig->odest[i].name,dest);
+        for(i=0; (i<p_orig->len_array && cmpDest!=0); i++) {
+            cmpDest = strcmp(p_orig->odest[i].name,dest);
         }
+        if(cmpDest == 0) {
 
-        if(pointer == NULL || strcmp(pointer->id_rel,rel)!=0){
-            //new
-            item  = malloc(sizeof(typeRel));
-            strcpy(item->id_rel,rel);
-            item->len_array=1;
-            strcpy(item->dest[0].name,dest);
-            item->dest[0].n_rel=1;
-            item->next=NULL;
+            cmpRel = strcmp(p_orig->odest[i].rel[j].id,rel);
+            for(j = 0; (j<p_orig->odest[i].len_array && cmpRel != 0);j++) {
+                cmpRel = strcmp(p_orig->odest[i].rel[j].id,rel);
+            }
+            if(cmpRel == 0) {
+                exist = 1;
+                printf("relazione già esistente\n");
+            }
+        }else {
+            strcpy(p_orig->odest[i].name,dest);
+            p_orig->len_array++;
+        }
+        if(exist == 0) {
+            strcpy(p_orig->odest[i].rel[j].id,rel);
+            p_orig->odest[i].len_array++;
 
-            if (pointer == NULL) {
-                array_lex[pos]=item;
-            }else {
-                if(strcmp(pointer->id_rel,rel)<0){ //aggiunta in coda
-                    pointer->next = item;
+            //analyze typeRel
+            printf("\nREL: %s\n",rel);
+            pos = (int) rel[0] - 45; //ASCII value of first char admitted is 45
+            pointer=array_lex[pos];
+
+            while (pointer != NULL && pointer->next != NULL && strcmp(pointer->id_rel,rel)<0) {
+                prec = pointer;
+                pointer = pointer->next;
+            }
+
+            if(pointer == NULL || strcmp(pointer->id_rel,rel)!=0){
+                //new
+                item  = malloc(sizeof(typeRel));
+                strcpy(item->id_rel,rel);
+                item->len_array=1;
+                strcpy(item->dest[0].name,dest);
+                item->dest[0].n_rel=1;
+                item->next=NULL;
+
+                if (pointer == NULL) {
+                    array_lex[pos]=item;
                 }else {
-                    item->next = pointer;
-                    if(prec == NULL) { //aggiunta in testa
-                        array_lex[pos]=item;
-                    }else { //aggiunta nel mezzo
-                        prec->next=item;
+                    if(strcmp(pointer->id_rel,rel)<0){ //aggiunta in coda
+                        pointer->next = item;
+                    }else {
+                        item->next = pointer;
+                        if(prec == NULL) { //aggiunta in testa
+                            array_lex[pos]=item;
+                        }else { //aggiunta nel mezzo
+                            prec->next=item;
+                        }
                     }
                 }
+                printf("nuova rel\n");
+            }else {
+                if(pointer->len_array>0){
+                    cmpTRel = strcmp(pointer->dest[z].name,dest);
+                    for(z=0;z<pointer->len_array && cmpTRel!=0;z++) {
+                        cmpTRel = strcmp(pointer->dest[z].name,dest);
+                    }
+                    if(cmpTRel==0){
+                        pointer->dest[z].n_rel++;
+                    }
+                }
+                pointer->dest[z].n_rel=1;
+                strcpy(pointer->dest[z].name,dest);
+                pointer->len_array++;
+
+                int index = z;
+                while(index>0 && (pointer->dest[z].n_rel>pointer->dest[index-1].n_rel || (pointer->dest[z].n_rel==pointer->dest[index-1].n_rel && strcmp(pointer->dest[z].name,pointer->dest[index-1].name)<0))) {
+                    index--;
+                }
+                ent_dest temp_ent = pointer->dest[z];
+                while(z!=index){
+                    pointer->dest[z]=pointer->dest[z-1];
+                    z--;
+                }
+                pointer->dest[index]=temp_ent;
+
+                printf("modificata rel\n");
             }
 
         }
-        printf("nuova rel\n");
-    }else {
-                //modifica
-                printf("modifica struttura\n");
-            }
-    }else printf("entità non monitorate\n");
+
+
+}else printf("entità non monitorate\n");
 
 }
 
 void delent(){
     int i,j,z;
     int pos;
+    int cmpUser;
     entity *prec_u=NULL;
     entity *user;
     typeRel *pointer;
+    typeRel *thisRel
     char username[1024];
     read(username);
 
@@ -210,18 +279,36 @@ void delent(){
                    hash[pos]=user->next;
                 }else hash[pos]=NULL;
             }
-             //eliminare tutte le relazioni di quell'utente
-            if(cacheEnt!=NULL)user->next=cacheEnt;
+
+            for(int u=0;u<user->len_array;u++) {
+                for (int w = 0; w<user->odest[u].len_array;w++) {
+                    findRel(user->odest[u].rel[w].id,thisRel);
+                    if(thisRel!= NULL){
+                        //trova nel dest array di thisrel odest[u].name, decrementa e riordina
+
+                    }
+                }
+            }
+            if(cacheEnt!=NULL) {
+                user->next=cacheEnt;
+            }
             cacheEnt = user;
             user = NULL;
 
             for (i = 0; i < 78; i++) {
                 pointer = array_lex[i];
                 while (pointer != NULL) {
-                    for (j = 0; j < pointer->len_array && strcmp(pointer->dest[j].name, username) != 0; j++) {}
-                    if (strcmp(pointer->dest[j].name, username) == 0) {
+                    printf("1 %s\n",pointer->id_rel);
+                    cmpUser = strcmp(pointer->dest[0].name, username);
+                    for (j = 0; j < pointer->len_array && cmpUser!= 0; j++) {
+                        printf("#\n");
+                        cmpUser = strcmp(pointer->dest[j].name, username);
+                    }
+                    if (cmpUser == 0) {
+                        printf("2\n");
                         pointer->len_array--;
                         for (z = j; z < pointer->len_array; z++) {
+                            printf("3\n");
                             pointer->dest[z] = pointer->dest[z + 1];
                         }
                     }
@@ -273,7 +360,7 @@ void end(){
 
 int main() {
     char action[10];
-
+    typeRel *p;
     do{
 
         scanf("%s", action);
@@ -295,6 +382,7 @@ int main() {
             end();
         }
     } while(action[0]!='e');
+
 
     printf("end of file\n");
 }
