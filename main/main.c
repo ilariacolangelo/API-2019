66005a7f73ff67c2ba2fa701c325162185316933
Ilaria Colangelo <ilaria.colangelo@mail.polimi.it>
2019-08-30 16:27:37 +0200

    ADDREL 2.0 w/ segfault

--- main/main.c
+++ main/main.c
@@ -5,32 +5,42 @@
 #include <stdbool.h>
 
 #define SIZEHASH 10000
-#define SIZETYPEREL 200
-#define SIZEDEST 10000
-#define SIZEORIG 10000
 
-typedef struct origEnt {
+typedef struct head_in_hash {
+    struct headReport *p;
+    struct head_in_hash *next;
+}head_in_hash;
+
+typedef struct orig_in_hash {
+    struct origRel *p;
+    struct orig_in_hash *next;
+}orig_in_hash;
+
+typedef struct origRel {
     struct entity *name;
-    struct origEnt *next;
-}origEnt;
+    struct headReport *dest;
+    struct origRel *prec;
+    struct origRel *next;
+}origRel;
 
 typedef struct headReport{                                 // entity in hashmap
     int n_rel;
     struct entity *name;
-    origEnt *first_orig;
+    origRel *first_orig;
+    struct headReport *prec;
     struct headReport *next;
 }headReport;
 
-typedef struct ent{                                 // entity in hashmap
+typedef struct entity{                                 // entity in hashmap
     char name[1024];
-    headReport *head_list;
-    origEnt *orig_list;
-    struct ent *next;
+    head_in_hash *head_list;
+    orig_in_hash *orig_list;
+    struct entity *next;
 }entity;
 
 typedef struct typeRel{                             //tipi relazioni
     char id_rel[1024];
-    headReport *rep_list;
+    headReport *head_list;
     struct typeRel *next;
 }typeRel;
 
@@ -38,6 +48,11 @@ typedef struct typeRel{                             //tipi relazioni
 entity *hash[SIZEHASH] = {NULL};
 typeRel *array_lex[78] = {NULL};
 entity *cacheEnt = NULL;
+headReport *cacheHead = NULL;
+origRel *cacheOrig = NULL;
+orig_in_hash *cacheHashOrig = NULL;
+head_in_hash *cacheHashHead = NULL;
+
 
 
 int hashfunc(char username[]){                      //polynomial rolling hash function
@@ -73,11 +88,13 @@ void findInHash(char username[],entity **p) {
     }
 }
 
-void findRel(char id_rel[], typeRel **p) {
+void findRel(char id_rel[], typeRel **p, typeRel **prec) {
     int pos=(int)id_rel[0]- 45;
+    (*prec)=NULL;
     (*p)=array_lex[pos];
     if ((*p)!= NULL){
         while ((*p)->next!=NULL && strcmp((*p)->id_rel,id_rel)<0){
+            (*prec)=(*p);
             (*p) = (*p)->next;
         }
         if(strcmp((*p)->id_rel,id_rel)!=0) {
@@ -137,7 +154,210 @@ void addent(){
     }else printf("presente %s\n",username);
 }
 
-void addrel() {}
+void addrel() {
+    char orig[1024];
+    char dest[1024];
+    char rel[1024];
+    int pos;
+
+    entity *p_orig;
+    entity *p_dest;
+    typeRel *p_rel;
+    typeRel *prec_rel;
+    typeRel *item_rel = NULL;
+    headReport *item_head = NULL;
+    headReport *temp_head = NULL;
+    headReport *index_head = NULL;
+    origRel *item_orig = NULL;
+    origRel *temp_orig = NULL;
+    orig_in_hash *item_hash_orig = NULL;
+    head_in_hash *item_hash_head = NULL;
+
+    read(orig);
+    read(dest);
+    read(rel);
+
+    findInHash(dest, &p_dest);      //cerco in hash dest e orig
+    findInHash(orig, &p_orig);
+
+    if (p_dest!= NULL && p_orig!=NULL) {    //entità presenti in hash
+
+        findRel(rel, &p_rel, &prec_rel);    //cerco typeRel
+        printf("rel found %s\n",p_rel->id_rel);
+
+        if(p_rel!=NULL){    //typeRel già presente
+            printf("da modificare \n");
+            temp_head = p_rel->head_list;
+            while (temp_head!=NULL && temp_head->next!=NULL && temp_head->name != p_dest){          // cerca il dest nella lista di headReport
+                temp_head = temp_head->next;
+            }
+            if(temp_head!=NULL && temp_head->name == p_dest) {         //trovato dest
+                temp_orig = temp_head->first_orig;
+                while (temp_orig!=NULL && temp_orig->next!=NULL && temp_orig->name != p_orig){          // cerca orig nella lista di origEnt
+                    temp_orig= temp_orig->next;
+                }
+                if(temp_orig != NULL && temp_orig->name == p_orig) {    //trovato orig
+                  printf("relazione già esistente\n");
+                    return;
+                }
+            }else {
+                if(cacheHead == NULL) {                     //mi creo elemento per headReport controllando la cacheHead
+                    item_head = malloc(sizeof(headReport));
+                }else {
+                    item_head = cacheHead;
+                    cacheHead = item_head->next;
+                }
+
+                item_head->n_rel = 0;                       //riempo e posiziono la headReport
+                item_head->next = NULL;
+                item_head->name = p_dest;
+
+                if(temp_head == NULL) {
+                    p_rel->head_list = item_head;
+                    item_head->prec = NULL;
+                }
+                else {                              //temp_head punta alla coda della lista
+                    temp_head->next = item_head;
+                    item_head->prec = temp_head;
+                }
+
+                if(cacheHashHead == NULL) {                     //mi creo elemento per head_in_hash controllando la cache
+                    item_hash_head = malloc(sizeof(head_in_hash));
+                }else {
+                    item_hash_head = cacheHashHead;
+                    cacheHashHead = item_hash_head->next;
+                }
+
+                item_hash_head->p = item_head;                  //aggancio entity alla struttura delle rel
+                item_hash_head->next=p_dest->head_list;
+                p_dest->head_list = item_hash_head;
+
+                temp_head = item_head; //temp punta al dest
+            }
+
+            //aggiungo orig
+            if(cacheOrig == NULL) {                     //mi creo elemento per origRel controllando la cacheOrig
+                item_orig = malloc(sizeof(origRel));
+            }else {
+                item_orig = cacheOrig;
+                cacheOrig = item_orig->next;
+            }
+
+            item_orig->dest = temp_head;                //riempo origRel
+            item_orig->name = p_orig;
+            item_orig->prec = NULL;
+            item_orig->next = temp_head->first_orig;    // aggiunto in testa
+            temp_head->first_orig = item_orig;
+            item_orig->next->prec = item_orig;
+
+            if(cacheHashOrig == NULL) {                     //mi creo elemento per orig_in_hash controllando la cache
+                item_hash_orig = malloc(sizeof(orig_in_hash));
+            }else {
+                item_hash_orig = cacheHashOrig;
+                cacheHashOrig = item_hash_orig->next;
+            }
+
+            item_hash_orig->p = item_orig;                  //aggancio entity alla struttura delle rel
+            item_hash_orig->next = p_orig->orig_list;
+            p_orig->orig_list = item_hash_orig;
+
+            temp_head->n_rel++;
+
+            //RIORDINAMENTO LISTA REPORT
+            index_head = temp_head;
+            while(index_head->prec != NULL && (temp_head->n_rel>index_head->prec->n_rel|| (temp_head->n_rel==index_head->prec->n_rel && strcmp(temp_head->name->name,index_head->prec->name->name)<0))) {
+                index_head = index_head->prec;
+            }
+
+            if(index_head->prec != NULL) { //aggiungo in testa
+                temp_head->prec->next = NULL;
+                temp_head->prec = NULL;
+                temp_head->next = index_head;
+                index_head->prec = temp_head;
+                p_rel->head_list = temp_head;
+
+            }else if(index_head != temp_head) { //aggiungo in mezzo
+                temp_head->prec->next = NULL;
+                temp_head->prec = index_head->prec;
+                index_head->prec->next = temp_head;
+                temp_head->next = index_head;
+                index_head->prec = temp_head;
+            }
+            // LISTA REPORT MODIFICATA
+
+            printf("modificata \n");
+        }else {     //creo nuovo typeRel
+            item_rel = malloc(sizeof(typeRel));         //mi creo elemento per typeRel
+
+            if(cacheHead == NULL) {                     //mi creo elemento per headReport controllando la cacheHead
+                item_head = malloc(sizeof(headReport));
+            }else {
+                item_head = cacheHead;
+                cacheHead = item_head->next;
+            }
+
+            if(cacheOrig == NULL) {                     //mi creo elemento per origRel controllando la cacheOrig
+                item_orig = malloc(sizeof(origRel));
+            }else {
+                item_orig = cacheOrig;
+                cacheOrig = item_orig->next;
+            }
+
+            strcpy(item_rel->id_rel,rel);//riempo e posiziono typeRel
+            item_rel->head_list = item_head;
+            item_rel->next = NULL;
+
+            findRel(rel,&p_rel,&prec_rel);
+            if (prec_rel == NULL) { //aggiungi in testa
+                pos=(int)rel[0]- 45;
+                if(array_lex[pos] != NULL) item_rel->next = array_lex[pos];
+                array_lex[pos] = item_rel;
+
+            }else {//aggiungi in mezzo o coda
+                item_rel->next = prec_rel->next;
+                prec_rel->next = item_rel;
+            }
+
+            item_head->n_rel = 1;                       //riempo headReport
+            item_head->next = NULL;
+            item_head->prec = NULL;
+            item_head->name = p_dest;
+            item_head->first_orig = item_orig;
+
+            item_orig->dest = item_head;                //riempo origRel
+            item_orig->prec = NULL;
+            item_orig->next = NULL;
+            item_orig->name = p_orig;
+
+            if(cacheHashHead == NULL) {                     //mi creo elemento per head_in_hash controllando la cache
+                item_hash_head = malloc(sizeof(head_in_hash));
+            }else {
+                item_hash_head = cacheHashHead;
+                cacheHashHead = item_hash_head->next;
+            }
+
+            if(cacheHashOrig == NULL) {                     //mi creo elemento per orig_in_hash controllando la cache
+                item_hash_orig = malloc(sizeof(orig_in_hash));
+            }else {
+                item_hash_orig = cacheHashOrig;
+                cacheHashOrig = item_hash_orig->next;
+            }
+
+            item_hash_head->p = item_head;                  //aggancio entity alla struttura delle rel
+            item_hash_head->next=p_dest->head_list;
+            p_dest->head_list = item_hash_head;
+
+            item_hash_orig->p = item_orig;                  //
+            item_hash_orig->next = p_orig->orig_list;
+            p_orig->orig_list = item_hash_orig;
+
+            printf("rel aggiunta\n");
+        }
+
+    }else {
+        printf("entità non monitorate\n");
+    }
+}
 
 void delent(){}
 
